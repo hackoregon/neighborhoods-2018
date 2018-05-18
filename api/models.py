8cf02272268bbce5ea02f49e5ec2f9c9917badcc
Andrew <andrewaccuardi@gmail.com>
2018-05-18 07:14:51 -0700

    fix spacing

--- api/models.py
+++ api/models.py
@@ -607,7 +607,6 @@ class Demolitions(models.Model):
         db_table = 'demolitions'
 
 
-
 class HousingAreas(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
