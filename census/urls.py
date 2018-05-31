4ff090a3be65379521b13cb9f645154e7cc028d7
Andrew <andrewaccuardi@gmail.com>
2018-05-30 18:11:51 -0700

    Change urls to be less redundant

--- census/urls.py
+++ census/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path('census_2000', views.Census2000List.as_view()),
-    path('census_2010', views.Census2010List.as_view())
+    path('2000', views.Census2000List.as_view()),
+    path('2010', views.Census2010List.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
