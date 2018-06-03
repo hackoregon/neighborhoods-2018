1e65e2c8e0e4b5f4b968cb4422ba3ad14db19da6
Andrew <andrewaccuardi@gmail.com>
2018-06-03 12:46:56 -0700

    change url for el_blockgroups

--- api/urls.py
+++ api/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('voter_precincts', views.VoterPrecinctsList.as_view()),
     path('zip_codes', views.ZipCodesList.as_view()),
     path('zoning', views.ZoningList.as_view()),
-    path('blockgroups_el', views.BlockgroupsElList.as_view()),
+    path('evictions_blockgroups', views.ElBlockgroupsList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
